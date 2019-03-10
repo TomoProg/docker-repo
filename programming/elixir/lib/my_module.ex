b4a97f1be4c79c2058ae504f942f6f379dca889e
TomoProg <helloworld0306.xxx@gmail.com>
2019-03-10 23:15:10 +0900

    Mix Project Sample

---
+++ programming/elixir/lib/my_module.ex
@@ -0,0 +1,18 @@
+defmodule MyModule do
+  @moduledoc """
+  Documentation for Elixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyModule.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- programming/elixir/work/hello.exs
+++
@@ -1,7 +0,0 @@
-defmodule Main do
-  def main do
-    IO.puts "Hello Elixir World!"
-  end
-end
-
-Main.main
